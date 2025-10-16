6948fda0de1de99c7d026692c4f5dc927a908aa5
Ashish <ashishsindhav02@outlook.com>
2025-10-16 23:23:04 +0530

    backend=02: register api with mongoDB and cloudinary

--- backend-02/src/index.js
+++ backend-02/src/index.js
@@ -5,6 +5,10 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+dotenv.config({
+    path: './.env'
+})
+
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
--- backend-02/src/utils/cloudinary.js
+++ backend-02/src/utils/cloudinary.js
@@ -4,21 +4,23 @@ import fs from "fs";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.envCLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
+        
         // upload the file on cloudinary
-        const response = await cloudinary.uploader.upload("", {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
+        // console.log("Cloudinary Response", response)
         // file has been uploaded successful
-        console.log("File is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
-        fs.unlinkSync() // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
